62c0ad43c5e2274c7a2109c2c733f47f0914f636
RGSSmart <85480372+RGSSmart@users.noreply.github.com>
2022-03-04 09:06:32 +0100

    III3

--- priprema za kontrolni/zadaci/grupa_B.js
+++ priprema za kontrolni/zadaci/grupa_B.js
@@ -17,3 +17,27 @@ var users = [{
 // 2. Napisati skrkptu koja uporedjuje datume 
 
 // 3. XAOMI PROBLEM - najbolji odnos cene i kvaliteta
+var uredjaji = [
+    {
+        ime: "A",
+        cena: 1500,
+        kvalitet: 4.5
+    },
+    {
+        ime: "B",
+        cena: 1800,
+        kvalitet: 4.0
+    },
+    {
+        ime: "C",
+        cena: 1400,
+        kvalitet: 4.2
+    }
+]
+var najbolji=uredjaji[0];
+uredjaji.forEach(function (value){
+    if(najbolji.cena/najbolji.kvalitet>value.cena/value.kvalitet)
+    najbolji=value;
+})
+
+najbolji
