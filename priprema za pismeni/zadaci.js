e7420f68bd4177bd6cd0bdc36a5c1290cc0bb894
RGSSmart <85480372+RGSSmart@users.noreply.github.com>
2022-04-27 10:59:44 +0200

    Rutiranje

--- priprema za pismeni/zadaci.js
+++ priprema za pismeni/zadaci.js
@@ -3,7 +3,7 @@
 // 1. Rutiranje
 // U electron-sablon-projekta dodati novu HTML stranicu koja se zove statistika.html. Na tu stranicu se dolazi linkom ‘/statistika’ koji se nalazi na pocetnoj stranici.
 // Statistika.html stranica je stranica koja sadrzi sledece statistike :
-// a) Koliko graf ima cvorova I veza
+// (a) Koliko graf ima cvorova I veza
 // b)* Provera da li je graf orijentisan ili nije
 // c) Proverava da li postoji cvor u grafu koji nije ni sa kim povezan
 // d) Proverava da li je graf kompletan ( da je svaki cvor spojen sa svakim drugim cvorom )
